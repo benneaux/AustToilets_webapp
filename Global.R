b7576b3aa7313379c5d11584bb12f7fe94dd34f4
benneaux <benjamin.g.moran@gmail.com>
2016-07-07 11:58:33 +1000

    Removed import step. Global.R now just reads from the Data file on Github.

--- Global.R
+++ Global.R
@@ -1,8 +1,4 @@
 library(dplyr)
 library(magrittr)
 
-toiletimport <- read.csv("Data\\toilet_data.csv", stringsAsFactors = TRUE)
-
-saveRDS(toiletimport, file="Data\\toiletdata.rds")
-
-toiletdata <- readRDS(file="Data\\toiletdata.rds")
+toiletdata <- readRDS(file="Data/toiletdata.rds")
