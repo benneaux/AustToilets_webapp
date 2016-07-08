be89ea70a9434dd26f93621e9520a37f59882c40
benneaux <benjamin.g.moran@gmail.com>
2016-07-08 12:39:48 +1000

    Create a table ('cleantable') with required fields.

--- Global.R
+++ Global.R
@@ -1,3 +1,24 @@
 library(dplyr)
 
 toiletdata <- readRDS(file="Data/toiletdata.rds")
+
+cleantable <- toiletdata %>%
+  select(
+    ID = ToiletID,
+    Name = Name,
+    Address = Address1,
+    Town = Town,
+    State = State,
+    Postcode = Postcode,
+    Male = Male,
+    Female = Female,
+    Unisex = Unisex,
+    BabyChange = BabyChange,
+    Showers = Showers,
+    DrinkingWater = DrinkingWater,
+    SharpsDisposal = SharpsDisposal,
+    SanitaryDisposal = SanitaryDisposal,
+    IconAltText = IconAltText,
+    latitude = Latitude,
+    longitude = Longitude
+  )
