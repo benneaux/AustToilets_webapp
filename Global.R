0b21092b9f0523fe5e3012e76aaec7181ed71829
benneaux <benjamin.g.moran@gmail.com>
2016-07-12 13:28:47 +1000

    removed NAs from toiletdata.rds.

--- Global.R
+++ Global.R
@@ -5,6 +5,5 @@ library(tidyr)
 # taxdata <- readRDS(file = "Data/taxdata.rds")
 # allpostcodes <- readRDS(file = "Data/allpostcodes.rds")
 toiletdata <- readRDS(file="Data/toiletdata.rds")
-toiletdata <- na.omit(toiletdata)
 
 
