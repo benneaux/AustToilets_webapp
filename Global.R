2b71e832b61d0a32d2983e1381c322ad2a24dfed
benneaux <benjamin.g.moran@gmail.com>
2016-07-12 15:50:11 +1000

    Added code to Global.R for importing fresh data.

--- Global.R
+++ Global.R
@@ -6,4 +6,32 @@ library(tidyr)
 # allpostcodes <- readRDS(file = "Data/allpostcodes.rds")
 toiletdata <- readRDS(file="Data/toiletdata.rds")
 
-
+# Code for importing data incase I screw it up.
+# toiletdata.import <- read.csv("Data/toilet_data.csv")
+# allpostcodes <- readRDS("Data/allpostcodes.rds")
+# 
+# toiletdata.import <- dplyr::inner_join(toiletdata.import, statecodes, by = c("State" = "state.name"))
+# 
+# toiletdata.import <- select(toiletdata.import,
+#                             -State,
+#                             -ToiletType,
+#                             -AddressNote,
+#                             -FacilityType,
+#                             -ToiletType,
+#                             -AccessNote,
+#                             -ParkingNote,
+#                             -AccessibleNote,
+#                             -AccessibleParkingNote,
+#                             -OpeningHoursSchedule,
+#                             -OpeningHoursNote,
+#                             -Notes,
+#                             -hover)
+# 
+# toiletdata.import <- dplyr::rename(toiletdata.import, address = Address1)
+# 
+# toiletdata <- toiletdata.import
+# 
+# #toiletdata <- plyr::join(toiletdata, allpostcodes, by = c("suburb", "state"))
+# 
+# 
+# saveRDS(toiletdata, file = "Data/toiletdata.rds")
